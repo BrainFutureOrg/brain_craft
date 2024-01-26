2068396156927da91defcf45922147a4966d8456
KosenkoAlexander <alexandkosenko@gmail.com>
2024-01-26 20:11:21 +0200

    output node implemented

--- src/neural_bfo/nodes/output_nodes.c
+++ src/neural_bfo/nodes/output_nodes.c
@@ -3,3 +3,14 @@
 //
 
 #include "output_nodes.h"
+
+void* output_node_forward_prop(node *output_node, void *args){
+    ((struct output_node_data*)output_node->internal_data)->data=args;
+    return NULL;
+}
+
+node* create_output_node(){
+    node *result= calloc(1, sizeof(node));
+    result->forward_prop=output_node_forward_prop;
+    return result;
+}
--- src/neural_bfo/nodes/output_nodes.h
+++ src/neural_bfo/nodes/output_nodes.h
@@ -5,4 +5,14 @@
 #ifndef BRAIN_CRAFT_SRC_NEURAL_BFO_NODES_OUTPUT_NODES_H
 #define BRAIN_CRAFT_SRC_NEURAL_BFO_NODES_OUTPUT_NODES_H
 
+#include "../../garbage_collector_bfo/self_free.h"
+#include "node.h"
+
+struct output_node_data{
+    void* data;
+};
+
+//creates output node
+node* create_output_node();
+
 #endif //BRAIN_CRAFT_SRC_NEURAL_BFO_NODES_OUTPUT_NODES_H
