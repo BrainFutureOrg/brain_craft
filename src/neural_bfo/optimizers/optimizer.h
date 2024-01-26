4412be10ec332eafd1bb57f9d497bd60027df6b5
KosenkoAlexander <alexandkosenko@gmail.com>
2024-01-26 20:13:10 +0200

    base optimizer struct remade

--- src/neural_bfo/optimizers/optimizer.h
+++ src/neural_bfo/optimizers/optimizer.h
@@ -5,4 +5,10 @@
 #ifndef BRAIN_CRAFT_SRC_NEURAL_BFO_OPTIMIZERS_OPTIMIZER_H
 #define BRAIN_CRAFT_SRC_NEURAL_BFO_OPTIMIZERS_OPTIMIZER_H
 
+#include "../../libraries/math_bfo/matrix_operations.h"
+
+typedef struct{
+    void (*optimization_step)(void* internal_data, matrix* to_change, matrix *gradient);
+    void *internal_data;
+}optimizer;
 #endif //BRAIN_CRAFT_SRC_NEURAL_BFO_OPTIMIZERS_OPTIMIZER_H
