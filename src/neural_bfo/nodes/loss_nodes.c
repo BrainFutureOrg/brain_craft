e9ca25cf699c7f8ee21caa93a76175899622baf0
KosenkoAlexander <alexandkosenko@gmail.com>
2024-01-26 20:00:36 +0200

    loss node implemented

--- src/neural_bfo/nodes/loss_nodes.c
+++ src/neural_bfo/nodes/loss_nodes.c
@@ -3,3 +3,44 @@
 //
 
 #include "loss_nodes.h"
+
+void* loss_forward_prop(node* loss_node, void *args){
+    struct loss_node_data* data=loss_node->internal_data;
+    struct order_enforcement *arg=args;
+    if(arg->order_number){
+        data->labels_self_free=arg->data;
+    }else{
+        data->predictions_self_free=arg->data;
+    }
+    free(arg);
+    if(data->predictions_self_free!=NULL&&data->labels_self_free!=NULL){
+        if(data->back_node!=NULL&&data->flags&1){
+            matrix* loss_d=data->loss_derivative(data->predictions_self_free->ptr_to_free, data->labels_self_free->ptr_to_free);
+            if(data->next==NULL){
+                free_if_unused_simple_small_matrix(data->predictions_self_free);
+                free_if_unused_simple_small_matrix(data->labels_self_free);
+            }
+            data->back_node->back_prop(data->back_node, create_self_free_simple_small(loss_d));
+        }
+        if(data->next!=NULL){
+            double* loss= calloc(1, sizeof(double));
+            *loss=data->loss_func(data->predictions_self_free->ptr_to_free, data->labels_self_free->ptr_to_free);
+            free_if_unused_simple_small_matrix(data->predictions_self_free);
+            free_if_unused_simple_small_matrix(data->labels_self_free);
+            data->next->forward_prop(data->next, loss);
+        }
+        data->predictions_self_free=NULL;
+        data->labels_self_free=NULL;
+    }
+    return NULL;
+}
+
+node* create_loss_node(double(*loss_func)(matrix*,matrix*), matrix*(*loss_derivative)(matrix*,matrix*)){
+    node *result= calloc(1, sizeof(node));
+    struct loss_node_data* data= calloc(1, sizeof(struct loss_node_data));
+    data->loss_func=loss_func;
+    data->loss_derivative=loss_derivative;
+    result->internal_data=data;
+    result->forward_prop=loss_forward_prop;
+    return result;
+}
--- src/neural_bfo/nodes/loss_nodes.h
+++ src/neural_bfo/nodes/loss_nodes.h
@@ -5,4 +5,23 @@
 #ifndef BRAIN_CRAFT_SRC_NEURAL_BFO_NODES_LOSS_NODES_H
 #define BRAIN_CRAFT_SRC_NEURAL_BFO_NODES_LOSS_NODES_H
 
+#include "../../libraries/math_bfo/matrix_operations.h"
+#include "node.h"
+#include "../../garbage_collector_bfo/self_free.h"
+#include "communication_structures.h"
+
+#define LOSS_NODE_FLAG_TRAIN 1
+struct loss_node_data{
+    double (*loss_func)(matrix* predictions, matrix* labels);
+    matrix* (*loss_derivative)(matrix* predictions, matrix* labels);
+    back_prop_node *back_node;
+    node *next;
+    self_free_simple_small *labels_self_free;
+    self_free_simple_small *predictions_self_free;
+    char flags;//1 = training
+};
+
+//creates loss node using loss function and it's derivative. Note that flags and connections to other nodes are not set
+node* create_loss_node(double(*loss_func)(matrix*,matrix*), matrix*(*loss_derivative)(matrix*,matrix*));
+
 #endif //BRAIN_CRAFT_SRC_NEURAL_BFO_NODES_LOSS_NODES_H
